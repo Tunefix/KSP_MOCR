19108575b550a23ce6ae0defd47f7439766a2663
Tunefix <github@tunefix.net>
2017-04-02 21:41:01 +0200

    -

--- KSP_MOCR/Form1.Designer.cs
+++ KSP_MOCR/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace KSP_MOCR
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(675, 482);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.KeyPreview = true;
