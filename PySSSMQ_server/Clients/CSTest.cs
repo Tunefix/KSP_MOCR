1892e5e7935162bd07a22bd3db3ebbdaa0cb05a9
Lars Andre <landas@gmail.com>
2017-04-04 21:25:48 +0200

    Added CSharp PySSSMQ_client

---
+++ PySSSMQ_server/Clients/CSTest.cs
@@ -0,0 +1,55 @@
+/**
+ *
+ * This is a test application to test the PySSSMQ_client
+ * Lars Andre Landås <landas@gmail.com>
+ *
+ */
+
+using System;
+
+public class CSTest {
+
+	static void Main() {
+
+		CSTestClient mainP = new CSTestClient();
+		mainP.Start();
+
+	}
+
+}
+
+public class CSTestClient {
+
+	public CSTestClient() {
+
+	}
+
+	public void Start() {
+
+		string key = "test_key";
+		string data = "testdata for key";
+
+		PySSSMQ_client client = new PySSSMQ_client();
+		if(!client.Connect("127.0.0.1")) {
+			Console.WriteLine("Could not connect to server");
+			return;
+		}
+
+		Console.WriteLine("Connected to server");
+
+		client.AttachReceiveEvent(this.ReceiveData);
+	
+		Console.WriteLine("this.Recieved is attached to PySSSMQ_client");
+		Console.WriteLine("Seding to server: key = " + key + ", data = " + data);
+
+		client.Send(key, data);
+
+		System.Threading.Thread.Sleep(1000);
+	}
+
+	public void ReceiveData(string key, string val) {
+		Console.WriteLine("ReceivedData: key = " + key + ", data = " + val);
+	}
+
+
+}
---
+++ PySSSMQ_server/Clients/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mcs -debug+ -out:CSTest.exe PySSSMQ_client.cs CSTest.cs
+
